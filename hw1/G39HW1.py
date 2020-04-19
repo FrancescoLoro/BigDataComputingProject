2b03c992f4fd0aae95f95548c3d45476cb9d0710
FrancescoLoro <francesco.official@gmail.com>
2020-04-19 11:12:11 +0200

    correct output format

--- hw1/G39HW1.py
+++ hw1/G39HW1.py
@@ -1,11 +1,9 @@
 from pyspark import SparkContext, SparkConf
 import sys
 import os
-import random as rand
 
 
 def class_count_per_doc(document):
-   # print(len(document.split("\n")))
     pairs_dict = {}
     for line in document.split("\n"):
         gamma = line.split(" ")[1]  # Takes just the class not the number
@@ -15,23 +13,6 @@ def class_count_per_doc(document):
             pairs_dict[gamma] += 1
     return [(gamma, pairs_dict[gamma]) for gamma in pairs_dict.keys()]
 
-
-
-def class_count_deterministic(docs, K):
-    def class_count_per_doc_random(document):
-        pairs_dict = {}
-        for line in document.split("\n"):
-            gamma = line.split(" ")[1]  # Takes just the class not the number
-            if gamma not in pairs_dict.keys():
-                pairs_dict[gamma] = 1
-            else:
-                pairs_dict[gamma] += 1
-        return [(gamma, pairs_dict[gamma]) for gamma in pairs_dict.keys()]
-
-    # <-- MAP PHASE (R1)
-    return sorted(docs.flatMap(class_count_per_doc_random).groupByKey().mapValues(len).collect())
-    # <-- REDUCE PHASE (R1)
-
 def count_in_a_partition(idx, iterator):
   count = 0
   for _ in iterator:
@@ -40,8 +21,11 @@ def count_in_a_partition(idx, iterator):
 
 def count_max_partition(docs):
     max_partition_size = docs.mapPartitionsWithIndex(count_in_a_partition).collect()
-    del max_partition_size[0::2]
+    del max_partition_size[0::2] #Remove index keep count
     return max(max_partition_size)
+
+def class_count_deterministic(docs):
+    return sorted(docs.flatMap(class_count_per_doc).groupByKey().mapValues(len).collect())# <-- MAP PHASE (R1) # <-- REDUCE PHASE (R1)
 
 
 def class_count_with_spark_partition(docs):
@@ -60,6 +44,7 @@ def class_count_with_spark_partition(docs):
                   .groupByKey()                              # <-- REDUCE PHASE (R2)
                   .mapValues(lambda vals: sum(vals)))
 
+    #Find most frequent class
     max_count = 0
     max_class = 0
     for c, count in word_count.collect():
@@ -87,13 +72,17 @@ def main():
     docs = sc.textFile(data_path, minPartitions=K).cache()
     docs = docs.repartition(numPartitions=K)
 
+    print("INPUT: \n\n** K =", K)
+    print("\n**DATASET:", data_path)
+    print("\nOUTPUT:\n")
+
     # CLASS COUNT
-    print("OUTPUT: \n\nVERSION WITH DETERMINISTIC PARTITIONS")
-    print("Number of distinct words in the documents = ",
-          str(class_count_deterministic(docs, K)))
+    print("VERSION WITH DETERMINISTIC PARTITIONS")
+    print("Output pairs = ",
+          str(class_count_deterministic(docs)))
 
     # CLASS COUNT WITH SPARK PARTITIONS
-    print("OUTPUT: \n\nVERSION WITH SPARK PARTITIONS\n")
+    print("VERSION WITH SPARK PARTITIONS")
     max_count, max_partition_size  = class_count_with_spark_partition(docs)
     print("Most frequent class = ", str(max_count))
     print("Max partition size = ", max_partition_size)
