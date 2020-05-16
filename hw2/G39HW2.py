63dfc34ed3423f72a3d779a645c36ac94bc77335
Luca <luca.perali@studenti.unipd.it>
2020-05-16 10:17:06 +0200

    random.choise the first centroid in Farthest First Traversal, working1

--- hw2/G39HW2.py
+++ hw2/G39HW2.py
@@ -23,7 +23,7 @@ def exactMPD(S):
     """
         receives in input a set of points S and returns the max distance between two points in S.
 
-    :param S: a set of points S.
+    :param S: a set of points S, each point is a tuple.
     :return: the max distance between two points in S.
     """
     max_dist = 0
@@ -45,18 +45,19 @@ def twoApproxMPD(S, k):
 
     :param S: set of points
     :param k: number of points
-    :return: maximum distance over between k points and all S points
+    :return: maximum distance over between k points and all S points,
+                zero if k is zero, raise ValueError if S has no element
     """
-    assert k < len(S), "k is less than |S|"
+    assert k < len(S), "k >= |S|"
     max_dist = 0
     random.seed(1237770)
-    centroids = random.sample(S, k)
+    centroids = random.sample(S, k)  # select k random centroids without repetition from S
     for ci in centroids:
         for sj in S:
-            cur_dist = quad_distance(ci, sj)
+            cur_dist = quad_distance(ci, sj)  # compare squared distances
             if max_dist < cur_dist:
                 max_dist = cur_dist
-    return math.sqrt(max_dist)
+    return math.sqrt(max_dist)  # root of squared distance == euclidean distance
 
 
 def kCenterMPD(S, k):
@@ -69,27 +70,28 @@ def kCenterMPD(S, k):
     :param k: number of points
     :return: Farthest-First Traversal algorithm result
     """
-    assert k < len(S), "k is less than |S|"
-    C = [S[0]]                          # first centroid
+    assert k < len(S), "k < |S| is needed, but k >= |S| is found"
+    random.seed(1237770)
+    c0 = random.choice(S)
+    C = [c0]                            # first centroid randomly selected
     S_dist = [math.inf for si in S]     # init distances of si from C
-    sj_max = S[0]                       # first element with max distance
+    sj_max = c0                         # first element with max distance
     S_dist[0] = 0                       # d(S[0], C) is 0
 
-    for i in range(1, k):
+    for i in range(1, k):  # select other k-1 centroids with FFT algorithm
         max_distance = 0
         j_max = -1
 
         # Find the point sj ∈ S − C that maximizes d(sj, C)
         for j, sj in enumerate(S):
-            if S_dist[j] == 0:              # sj is in C, skip
+            if S_dist[j] == 0:              # sj is already in C, skip
                 continue
-            cur_dist = min(quad_distance(sj, C[-1]), S_dist[j])  # d(sj, C)
+            cur_dist = min(quad_distance(sj, C[-1]), S_dist[j])  # d(sj, C), C[-1] is the last added centroid
             S_dist[j] = cur_dist            # update sj distance from C
             if cur_dist > max_distance:     # check if sj is the farthest element from C
                 sj_max = sj                 # save the farthest element
                 max_distance = cur_dist     # update the max distance of the element
                 j_max = j                   # update the index of the farthest element
-        # assert sj_max not in C, "try to insert in C an element of C"
 
         # add the farthest element to centroid and update it's distance from C to 0
         C.append(sj_max)        # add the farthest element to C
@@ -98,6 +100,11 @@ def kCenterMPD(S, k):
 
 
 def quad_distance(p1, p2):
+    """
+    :param p1: a tuple of numbers
+    :param p2: a tuple of numbers, with the same length of p1
+    :return: squared distance between the points, zero if the tuple have no elements
+    """
     assert len(p1) == len(p2), "input points must have the same num of components"
     dist = 0
     for i in range(len(p1)):
@@ -106,6 +113,12 @@ def quad_distance(p1, p2):
 
 
 def euclidean(p1, p2):
+    """
+    Calculate the euclidean distance between two points
+    :param p1: a tuple of numbers
+    :param p2: a tuple of numbers, with the same length of p1
+    :return: euclidean distance between p1 and p2, zero if p1 and p2 has no elements
+    """
     return math.sqrt(quad_distance(p1, p2))
 
 
